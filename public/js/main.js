8c4e02a9bb4b4901c742a213412768f913dafe74
wnajar <will.najar@outlook.com>
2014-04-24 18:20:52 -0700

    fix readme

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var formattingbuttons = document.getElementsByClassName('formatting-button');
+
+	//Formatting buttons for threads and replies
+	var formattingbuttons = document.querySelectorAll('.formatting-button');
 	for(var i = 0; i < formattingbuttons.length; i++){
     	formattingbuttons[i].addEventListener('click', function() {
     		var action = this.getAttribute('data-action');
     		alert(action);
+    		// switch (action) {
+	     //        case "bold":
+	     //            wrapText(textarea, "[b]", "[/b]", "", "bolded text");
+	     //            break;
+	     //        case "italic":
+	     //            wrapText(textarea, "[i]", "[/i]", "", "italicized text");
+	     //            break;
+	     //        case 'link':
+	     //            wrapText(textarea, "[url]", "[/url]", "", "link url");
+	     //            break;
+	     //        case 'image':
+	     //            wrapText(textarea, "[img]", "[/img]", "", "image url");
+	     //            break;
+	     //        case 'youtube':
+	     //            wrapText(textarea, "[youtube]", "[/youtube]", "", "unique video id");
+	     //            break;
+	     //        case 'quote':
+	     //            wrapText(textarea, "[quote]", "[/quote]", "", "quoted text");
+	     //            break;
+	     //        case 'strikethrough':
+	     //            wrapText(textarea, "[s]", "[/s]", "", "spoiler text");
+	     //            break;
+	     //    }
     	}, false);
     }
 }, false);
--- readme.md
+++ readme.md
@@ -6,8 +6,8 @@ Ezra Hub is a forum for Cornell University students.
 
 ###Design goals
 
--“Simplicity is the ultimate sophistication.”
--Implicit is better than explicit
--Laravel 4.1 base
--Shh no jQuery... only pure JavaScript now
+* “Simplicity is the ultimate sophistication.”
+* Implicit is better than explicit
+* Laravel 4.1 base
+* Shh no jQuery... only pure JavaScript now
 
